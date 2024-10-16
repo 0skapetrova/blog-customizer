5972b8f8db9befc012c32911c0f5bdca4c5eda08
Olga Petrova <oskapetrovaa@yandex.ru>
2024-10-16 13:49:19 +0300

    исправлены замечания по ревью

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
+import { StrictMode } from 'react';
 
 import './styles/index.scss';
-import styles from './styles/index.module.scss';
-import { defaultCssVars, TCssVars } from './constants/articleProps';
+
+import { App } from './components/app/App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [cssVars, setCssVars] = useState<TCssVars>(defaultCssVars);
-	console.log(cssVars);
-	return (
-		<main className={clsx(styles.main)} style={cssVars as CSSProperties}>
-			<ArticleParamsForm
-				setCssVars={(state) => {
-					setCssVars(state);
-				}}
-			/>
-			<Article />
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
